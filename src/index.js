fd8b2668760894076c15c572dda23c9ca0264172
alexaaaant <alexaaaant@gmail.com>
2020-05-19 22:01:44 +0300

    Гистограмма d3

--- src/index.js
+++ src/index.js
@@ -1,51 +1,30 @@
-import { select, selectAll, max, scaleLinear } from 'd3'
+import { select, max, scaleLinear, csv, scaleBand } from 'd3'
 
-const buttons = selectAll("button").nodes()
 const container = select("#container")
 
-let clients = [
-    { name: "client0", weigth: 102 },
-    { name: `client 1`, weigth: 100 },
-    { name: `client 2`, weigth: 76 },
-    { name: `client 3`, weigth: 60 },
-    { name: `client 4`, weigth: 80 },
-    { name: `client 5`, weigth: 110 },
-]
-let count = 1;
+csv("data.csv").then(showData)
 
-buttons[0].addEventListener('click', add);
-buttons[1].addEventListener('click', remove);
-
-function add() {
-    clients.push(
-        { name: `client ${count}`, weigth: 100 },
-    );
-    count++;
-    showData(clients)
-}
-
-function remove() {
-    clients = clients.slice(0, -1);
-    count--;
-    showData(clients);
-}
 
 function showData(clients) {
-    const maxValue = max(clients, d => d.weigth);
-    const scale = scaleLinear()
+    const maxValue = max(clients, d => d.weight);
+
+    const widthScale = scaleLinear()
         .range([0, 300])
         .domain([0, maxValue])
 
-    let join = container
-        .selectAll("div")
-        .data(clients)
-    join.enter()
-        .append("div")
-        .text(d => `${d.name}: ${scale(d.weigth)}`)
-        .style("background-color", "blue")
-        .style("margin", "5px")
-        .style("color", "white")
-        .style("width", c => `${scale(c.weigth)}px`)
-}
+    const positionScale = scaleBand()
+        .range([0, 200])
+        .domain(clients.map(c => c.name))
+        .padding(0.1)
 
-showData(clients)
+    const join = container
+        .selectAll("rect")
+        .data(clients)
+
+    join.enter()
+        .append("rect")
+        .attr("fill", "blue")
+        .attr("width", c => widthScale(c.weight))
+        .attr("height", positionScale.bandwidth())
+        .attr("y", c => positionScale(c.name))
+}
