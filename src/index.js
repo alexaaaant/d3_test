ed665028adf3a7c282153c7c27c354d13251465e
alexaaaant <alexaaaant@gmail.com>
2020-05-19 21:21:00 +0300

    Рисование гистограммы

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import { select, selectAll, csv, ascending, sum, mean, extent } from 'd3'
+import { select, selectAll, max, scaleLinear } from 'd3'
 
 const buttons = selectAll("button").nodes()
 const container = select("#container")
 
-let clients = [{
-    name: "client0"
-}]
+let clients = [
+    { name: "client0", weigth: 102 },
+    { name: `client 1`, weigth: 100 },
+    { name: `client 2`, weigth: 76 },
+    { name: `client 3`, weigth: 60 },
+    { name: `client 4`, weigth: 80 },
+    { name: `client 5`, weigth: 110 },
+]
 let count = 1;
 
 buttons[0].addEventListener('click', add);
 buttons[1].addEventListener('click', remove);
 
 function add() {
-    clients.push({ name: `client ${count}` });
+    clients.push(
+        { name: `client ${count}`, weigth: 100 },
+    );
     count++;
     showData(clients)
 }
@@ -24,15 +31,21 @@ function remove() {
 }
 
 function showData(clients) {
+    const maxValue = max(clients, d => d.weigth);
+    const scale = scaleLinear()
+        .range([0, 300])
+        .domain([0, maxValue])
+
     let join = container
         .selectAll("div")
         .data(clients)
     join.enter()
         .append("div")
-        .text(c => c.name + " New")
-    join.text(c => c.name + " Updated")
-    join.exit().remove()
-    console.log(join)
+        .text(d => `${d.name}: ${scale(d.weigth)}`)
+        .style("background-color", "blue")
+        .style("margin", "5px")
+        .style("color", "white")
+        .style("width", c => `${scale(c.weigth)}px`)
 }
 
 showData(clients)
