e294901fd8756d9568f20828d7e8abd3f44c0115
alexaaaant <alexaaaant@gmail.com>
2020-05-22 18:50:46 +0300

    d3 svg

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { select, max, scaleLinear, csv, scaleBand } from 'd3'
+import { select, max, scaleLinear, csv, scaleBand, axisBottom, axisLeft } from 'd3'
 
 const container = select("#container")
 
 csv("data.csv").then(showData)
 
+let body = select("#body");
 
 function showData(clients) {
     const maxValue = max(clients, d => d.weight);
@@ -17,7 +18,7 @@ function showData(clients) {
         .domain(clients.map(c => c.name))
         .padding(0.1)
 
-    const join = container
+    const join = body
         .selectAll("rect")
         .data(clients)
 
@@ -27,4 +28,16 @@ function showData(clients) {
         .attr("width", c => widthScale(c.weight))
         .attr("height", positionScale.bandwidth())
         .attr("y", c => positionScale(c.name))
+
+    let xAxis = axisBottom(widthScale)
+        .ticks(5)
+        .tickFormat(d => d + " kg")
+
+    select("#xAxis")
+        .attr("transform", "translate(50, 200)")
+        .call(xAxis)
+    let yAxis = axisLeft(positionScale)
+    select("#yAxis")
+        .attr("transform", "translate(50, 0)")
+        .call(yAxis)
 }
