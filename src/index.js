674c895d4b4e765f56e6f58604c776318059562d
alexaaaant <alexaaaant@gmail.com>
2020-05-22 19:56:49 +0300

    бар airlines

--- src/index.js
+++ src/index.js
@@ -31,38 +31,91 @@ function groupByAirline(data) {
 }
 
 function showData() {
-    let routes = store.routes
     let airlines = groupByAirline(store.routes);
-    console.log(airlines)
+    drawAirlinesChart(airlines)
+}
+
+function getAirlinesChartConfig() {
+    let width = 350;
+    let height = 400;
+    let margin = {
+        top: 10,
+        bottom: 50,
+        left: 130,
+        right: 10
+    }
+
+    let bodyHeight = height - margin.top - margin.bottom
+    let bodyWidth = width - margin.left - margin.right
+
+    let container = select("#AirlinesChart");
+    container
+        .attr("width", width)
+        .attr("height", height)
+
+    return { width, height, margin, bodyHeight, bodyWidth, container }
+}
+
+function drawAirlinesChart(airlines) {
+    let config = getAirlinesChartConfig();
+    let scales = getAirlinesChartScales(airlines, config);
+    drawBarsAirlinesChart(airlines, scales, config)
+    drawAxesAirlinesChart(scales, config);
+}
+
+function getAirlinesChartScales(airlines, config) {
+    let { bodyWidth, bodyHeight } = config;
+    let maximunCount = max(airlines, v => v.Count)
+
+    let xScale = scaleLinear()
+        .range([0, bodyWidth])
+        .domain([0, maximunCount])
+
+    let yScale = scaleBand()
+        .range([0, bodyHeight])
+        .domain(airlines.map(a => a.AirlineName))
+        .padding(0.2)
+
+    return { xScale, yScale }
+}
+
+function drawBarsAirlinesChart(airlines, scales, config) {
+    let { margin, container } = config
+    let { xScale, yScale } = scales
+    let body = container.append("g")
+        .style("transform",
+            `translate(${margin.left}px,${margin.top}px)`
+        )
+
+    let bars = body.selectAll(".bar")
+        .data(airlines)
+
+    bars.enter().append("rect")
+        .attr("height", yScale.bandwidth())
+        .attr("y", (d) => yScale(d.AirlineName))
+        .attr("width", v => xScale(v.Count))
+        .attr("fill", "#2a5599")
+}
+
+function drawAxesAirlinesChart(scales, config) {
+    let { xScale, yScale } = scales
+    let { container, margin, height } = config;
+    let axisX = axisBottom(xScale)
+        .ticks(5)
+
+    container.append("g")
+        .style("transform",
+            `translate(${margin.left}px,${height - margin.bottom}px)`
+        )
+        .call(axisX)
+
+    let axisY = axisLeft(yScale)
+
+    container.append("g")
+        .style("transform",
+            `translate(${margin.left}px,${margin.top}px)`
+        )
+        .call(axisY)
 }
 
 loadData().then(showData);
-
-    const positionScale = scaleBand()
-        .range([0, 200])
-        .domain(clients.map(c => c.name))
-        .padding(0.1)
-
-    const join = body
-        .selectAll("rect")
-        .data(clients)
-
-    join.enter()
-        .append("rect")
-        .attr("fill", "blue")
-        .attr("width", c => widthScale(c.weight))
-        .attr("height", positionScale.bandwidth())
-        .attr("y", c => positionScale(c.name))
-
-    let xAxis = axisBottom(widthScale)
-        .ticks(5)
-        .tickFormat(d => d + " kg")
-
-    select("#xAxis")
-        .attr("transform", "translate(50, 200)")
-        .call(xAxis)
-    let yAxis = axisLeft(positionScale)
-    select("#yAxis")
-        .attr("transform", "translate(50, 0)")
-        .call(yAxis)
-}
