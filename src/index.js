58cf91ed806aeddc5e4f3ce2552edfc76d46fc2c
alexaaaant <alexaaaant@gmail.com>
2020-06-06 20:00:00 +0300

    d3 карта полетов

--- src/index.js
+++ src/index.js
@@ -1,162 +1,158 @@
 import * as d3 from 'd3'
 
-const body = d3.select("#body")
+const store = {};
 
-Promise.all([
-    d3.csv("data.csv"),
-    d3.json("countries.geo.json"),
-]).then(showData)
+async function loadData() {
+    return Promise.all([d3.csv("routes.csv"), d3.json("countries.geo.json")])
+        .then((datasets) => {
+            store.routes = datasets[0];
+            store.geoJSON = datasets[1];
+            return store;
+        })
+}
 
-function showData(datasources) {
-    const mapInfo = datasources[1];
-    const data = datasources[0];
+function groupByAirline(data) {
+    let result = data.reduce((result, d) => {
+        let currentData = result[d.AirlineID] || {
+            "AirlineID": d.AirlineID,
+            "AirlineName": d.AirlineName,
+            "Count": 0
+        }
 
-    const bodyH = 400
-    const bodyW = 400
+        currentData.Count++
 
-    let projection = d3.geoMercator()
-        .scale(100)
-        .translate([bodyW / 2, bodyH / 2])
+        result[d.AirlineID] = currentData;
 
-    let path = d3.geoPath()
-        .projection(projection)
+        return result;
+    }, {})
 
-    body.selectAll("path")
-        .data(mapInfo.features)
+    result = Object.keys(result).map(key => result[key])
+    result = result.sort((a, b) => b.Count - a.Count)
+    return result
+}
+
+function showData() {
+    let airlines = groupByAirline(store.routes);
+    drawAirlinesChart(airlines);
+    drawMap(store.geoJSON);
+}
+
+function getAirlinesChartConfig() {
+    let width = 350;
+    let height = 400;
+    let margin = {
+        top: 10,
+        bottom: 50,
+        left: 130,
+        right: 10
+    }
+
+    let bodyHeight = height - margin.top - margin.bottom
+    let bodyWidth = width - margin.left - margin.right
+
+    let container = d3.select("#AirlinesChart");
+    container
+        .attr("width", width)
+        .attr("height", height)
+
+    return { width, height, margin, bodyHeight, bodyWidth, container }
+}
+
+function drawAirlinesChart(airlines) {
+    let config = getAirlinesChartConfig();
+    let scales = getAirlinesChartScales(airlines, config);
+    drawBarsAirlinesChart(airlines, scales, config)
+    drawAxesAirlinesChart(scales, config);
+}
+
+function getAirlinesChartScales(airlines, config) {
+    let { bodyWidth, bodyHeight } = config;
+    let maximunCount = d3.max(airlines, v => v.Count)
+
+    let xScale = d3.scaleLinear()
+        .range([0, bodyWidth])
+        .domain([0, maximunCount])
+
+    let yScale = d3.scaleBand()
+        .range([0, bodyHeight])
+        .domain(airlines.map(a => a.AirlineName))
+        .padding(0.2)
+
+    return { xScale, yScale }
+}
+
+function drawBarsAirlinesChart(airlines, scales, config) {
+    let { margin, container } = config
+    let { xScale, yScale } = scales
+    let body = container.append("g")
+        .style("transform",
+            `translate(${margin.left}px,${margin.top}px)`
+        )
+
+    let bars = body.selectAll(".bar")
+        .data(airlines)
+
+    bars.enter().append("rect")
+        .attr("height", yScale.bandwidth())
+        .attr("y", (d) => yScale(d.AirlineName))
+        .attr("width", v => xScale(v.Count))
+        .attr("fill", "#2a5599")
+}
+
+function drawAxesAirlinesChart(scales, config) {
+    let { xScale, yScale } = scales
+    let { container, margin, height } = config;
+    let axisX = d3.axisBottom(xScale)
+        .ticks(5)
+
+    container.append("g")
+        .style("transform",
+            `translate(${margin.left}px,${height - margin.bottom}px)`
+        )
+        .call(axisX)
+
+    let axisY = d3.axisLeft(yScale)
+
+    container.append("g")
+        .style("transform",
+            `translate(${margin.left}px,${margin.top}px)`
+        )
+        .call(axisY)
+}
+
+function getMapConfig() {
+    const width = 600;
+    const height = 400;
+    const container = d3.select("#Map");
+    container
+        .style("height", height)
+        .style("width", width);
+    return { width, height, container }
+}
+
+function getMapProjection(config) {
+    const { height, width } = config;
+    const projection = d3.geoMercator();
+    projection.scale(97)
+        .translate([width / 2, height / 2 + 20])
+    store.mapProjection = projection;
+    return store.mapProjection;
+}
+
+function drawBaseMap(container, countries, projection) {
+    const path = d3.geoPath().projection(projection);
+    container.selectAll("path").data(countries)
         .enter()
         .append("path")
         .attr("d", d => path(d))
-        .attr("stroke", "#999")
+        .attr("stroke", "#ccc")
         .attr("fill", "#eee")
-
-    body.selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("r", 2)
-        .attr("fill", "#0055AA")
-        .style("opacity", 0.5) 
-        .attr("cx", d => projection([+d.Longitude, +d.Latitude])[0])
-        .attr("cy", d => projection([+d.Longitude, +d.Latitude])[1])
-
 }
 
+function drawMap(geoJson) {
+    const config = getMapConfig();
+    const projection = getMapProjection(config);
+    drawBaseMap(config.container, geoJson.features, projection);
+}
 
-// const store = {};
-
-// async function loadData() {
-//     let promise = csv("routes.csv")
-//     const routes = await promise;
-//     store.routes = routes;
-//     return store;
-// }
-
-// function groupByAirline(data) {
-//     let result = data.reduce((result, d) => {
-//         let currentData = result[d.AirlineID] || {
-//             "AirlineID": d.AirlineID,
-//             "AirlineName": d.AirlineName,
-//             "Count": 0
-//         }
-
-//         currentData.Count++
-
-//         result[d.AirlineID] = currentData;
-
-//         return result;
-//     }, {})
-
-//     result = Object.keys(result).map(key => result[key])
-//     result = result.sort((a, b) => b.Count - a.Count)
-//     return result
-// }
-
-// function showData() {
-//     let airlines = groupByAirline(store.routes);
-//     drawAirlinesChart(airlines)
-// }
-
-// function getAirlinesChartConfig() {
-//     let width = 350;
-//     let height = 400;
-//     let margin = {
-//         top: 10,
-//         bottom: 50,
-//         left: 130,
-//         right: 10
-//     }
-
-//     let bodyHeight = height - margin.top - margin.bottom
-//     let bodyWidth = width - margin.left - margin.right
-
-//     let container = select("#AirlinesChart");
-//     container
-//         .attr("width", width)
-//         .attr("height", height)
-
-//     return { width, height, margin, bodyHeight, bodyWidth, container }
-// }
-
-// function drawAirlinesChart(airlines) {
-//     let config = getAirlinesChartConfig();
-//     let scales = getAirlinesChartScales(airlines, config);
-//     drawBarsAirlinesChart(airlines, scales, config)
-//     drawAxesAirlinesChart(scales, config);
-// }
-
-// function getAirlinesChartScales(airlines, config) {
-//     let { bodyWidth, bodyHeight } = config;
-//     let maximunCount = max(airlines, v => v.Count)
-
-//     let xScale = scaleLinear()
-//         .range([0, bodyWidth])
-//         .domain([0, maximunCount])
-
-//     let yScale = scaleBand()
-//         .range([0, bodyHeight])
-//         .domain(airlines.map(a => a.AirlineName))
-//         .padding(0.2)
-
-//     return { xScale, yScale }
-// }
-
-// function drawBarsAirlinesChart(airlines, scales, config) {
-//     let { margin, container } = config
-//     let { xScale, yScale } = scales
-//     let body = container.append("g")
-//         .style("transform",
-//             `translate(${margin.left}px,${margin.top}px)`
-//         )
-
-//     let bars = body.selectAll(".bar")
-//         .data(airlines)
-
-//     bars.enter().append("rect")
-//         .attr("height", yScale.bandwidth())
-//         .attr("y", (d) => yScale(d.AirlineName))
-//         .attr("width", v => xScale(v.Count))
-//         .attr("fill", "#2a5599")
-// }
-
-// function drawAxesAirlinesChart(scales, config) {
-//     let { xScale, yScale } = scales
-//     let { container, margin, height } = config;
-//     let axisX = axisBottom(xScale)
-//         .ticks(5)
-
-//     container.append("g")
-//         .style("transform",
-//             `translate(${margin.left}px,${height - margin.bottom}px)`
-//         )
-//         .call(axisX)
-
-//     let axisY = axisLeft(yScale)
-
-//     container.append("g")
-//         .style("transform",
-//             `translate(${margin.left}px,${margin.top}px)`
-//         )
-//         .call(axisY)
-// }
-
-// loadData().then(showData);
+loadData().then(showData);
