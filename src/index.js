2930ebfb9e5dc5e8d17d89eabe77487cd4a34190
alexaaaant <alexaaaant@gmail.com>
2020-06-07 15:07:46 +0300

    network

--- src/index.js
+++ src/index.js
@@ -2,21 +2,57 @@ import * as d3 from 'd3'
 
 d3.json("nodes.json").then(showData)
 
+const body = d3.select("#body")
+
+function createElements(data) {
+    const nodes = body.append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(data.nodes)
+        .enter()
+        .append("circle")
+        .attr("r", 5)
+        .attr("fill", "black")
+
+    const links = body.append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(data.links)
+        .enter()
+        .append("line")
+        .attr("stroke", "black")
+}
+
+function updateElements() {
+    d3.select(".nodes")
+        .selectAll("circle")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+
+    d3.select(".links")
+        .selectAll("line")
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y)
+
+}
+
 function showData(data) {
-    console.log(data)
     const bodyHeight = 400;
     const bodyWidth = 400;
-
+    createElements(data);
     const simulation = d3.forceSimulation()
         .force("link", d3.forceLink().id((d) => d.id))
         .force("charge", d3.forceManyBody())
         .force("center", d3.forceCenter(bodyWidth / 2, bodyHeight / 2))
 
-    simulation.nodes(data.nodes)
-        .on("tick", d => {
-            console.log(data)
-        })
-    simulation.force("link").links(data.links)
+    simulation
+        .nodes(data.nodes)
+        .on("tick", updateElements)
+
+    simulation.force("link")
+        .links(data.links)
 }
 // const store = {};
 
